0369935b1ac91f8b4a480986664bb3c48e3ccd44
LizaKornilo <kornilo_ei_18@mf.grsu.by>
2022-03-09 20:57:18 +0300

    feat: implement admin page

--- src/pages/AdminPanel.tsx
+++ src/pages/AdminPanel.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import './AdminPanel.scss';
 
 function AdminPanel() {
+  const userEmail = useSelector((state: any) => state.user.email);
+
   return (
-    <div>
-      AdminPanel
+    <div className="admin-page">
+      <div className="container">
+        <div className="bigTitle">Админ панель</div>
+        <div className="subTitle">{` Пользователь: ${userEmail}`}</div>
+        <div className="admin-page__text">
+          Здесь может быть много инфы по покупателям, их корзинам и заказам
+        </div>
+      </div>
     </div>
   );
 }

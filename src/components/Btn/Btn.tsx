4604928698073acd569c9e4cfa41959e6a36e2cb
LizaKornilo <kornilo_ei_18@mf.grsu.by>
2022-03-09 20:56:14 +0300

    refactor: improve Btn component

--- src/components/Btn/Btn.tsx
+++ src/components/Btn/Btn.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import './Btn.scss';
 
-function Btn({ text, type }: any) {
+interface Props {
+  text: string,
+  type: string,
+  isSubmitType?: boolean;
+  onClick?: () => void,
+}
+
+function Btn({
+  text, type, isSubmitType, onClick,
+}: Props) {
+  if (isSubmitType === true) {
+    return (
+      <button
+        type="submit"
+        className={`myButton ${type}`}
+      >
+        {text}
+      </button>
+    );
+  }
   return (
-    <button className={`myButton ${type}`} type="button">{text}</button>
+    <button
+      type="button"
+      className={`myButton ${type}`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
   );
 }
+
+Btn.defaultProps = {
+  isSubmitType: false,
+  onClick: () => null,
+};
 
 export default Btn;

5b0b0090b2b1e85a0267e54dc6dff03b9b234b7a
LizaKornilo <kornilo_ei_18@mf.grsu.by>
2022-03-22 13:30:12 +0300

    feat/refactor: insert modals to Store and ProductPage, rename SCSS classes by CSS naming convention

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,52 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { Product } from '../common/models/product.dto';
 import Btn from '../components/Btn/Btn';
+import Modal from '../components/Modal/Modal';
+import AddToBasketModalContent from '../components/ModalContent/AddToBasketModalContent/AddToBasketModalContent';
+import DeleteModalContent from '../components/ModalContent/DeleteModalContent/DeleteModalContent';
+import AddEditModalContent from '../components/ModalContent/AddEditModalContent/AddEditModalContent';
 import SvgSelector from '../components/SvgSelector';
 import { Roles } from '../utils/consts';
 import './ProductPage.scss';
 
 function ProductPage() {
-  const productId = useParams().id;
+  const productId = Number(useParams().id);
 
-  const currentProduct = useSelector((state: any) => state.products.products)
-    .find((item: any) => item.id === Number(productId));
+  const currentProduct: Product = useSelector((state: any) => state.products.products)
+    .find((item: any) => item.id === productId);
   const userRole = useSelector((state: any) => state.user.role);
 
-  const showGuestAddToBasketPopup = () => {
-    console.log('GuestAddToBasket');
-  };
+  const [basketModalActive, setBasketModalActive] = useState(false);
+  const [editModalActive, setEditModalActive] = useState(false);
+  const [deleteModalActive, setDeleteModalActive] = useState(false);
 
-  const showAddToBasketPopup = () => {
-    console.log('AddToBasket');
+  const addProductToBasket = () => {
+    if (userRole !== Roles.GUEST) {
+      // Make request via Saga (currentProduct.id user.id)
+    }
+    setBasketModalActive(true);
   };
 
   return (
-    <div className="productPage">
+    <div className="product-page">
       <div className="container">
-        <div className="productPage__wrapper">
-          <div className="productImage">{currentProduct.image}</div>
+        <div className="product-page__wrapper">
+          {/* <div> */}
+          <img className="product-image" src={currentProduct.image} alt={`product ${currentProduct.id}`} />
+          {/* </div> */}
           <div className="product-page__info">
-            <div className="productInfo">
-              <div className="productName">{currentProduct.name}</div>
-              <div className="productPrice">{`${currentProduct.price} p.`}</div>
-              {
-                userRole === Roles.GUEST
-                  ? <Btn text="Добавить в корзину" type="dark" onClick={() => showGuestAddToBasketPopup()} />
-                  : <Btn text="Добавить в корзину" type="dark" onClick={() => showAddToBasketPopup()} />
-              }
-              <div className="productDescription">{currentProduct.description}</div>
+            <div className="product-info">
+              <div className="product-name">{currentProduct.name}</div>
+              <div className="product-price">{`${currentProduct.price} p.`}</div>
+
+              <Btn text="Добавить в корзину" type="dark" onClick={() => addProductToBasket()} />
+              <Modal active={basketModalActive} setActive={setBasketModalActive}>
+                <AddToBasketModalContent />
+              </Modal>
+
+              <div className="product-description">{currentProduct.description}</div>
             </div>
             {
               userRole === Roles.ADMIN
                 ? (
-                  <div className="product-page__admin-tools">
-                    <SvgSelector
-                      id="edit-pencil"
-                      classNames="admin-edit-pr-btn"
-                    />
-                    <SvgSelector id="delete-basket" classNames="admin-delete-pr-btn" />
-                  </div>
+                  <>
+                    <div className="product-page__admin-tools">
+                      <SvgSelector
+                        id="edit-pencil"
+                        classNames="admin-edit-pr-btn"
+                        onClick={() => setEditModalActive(true)}
+                      />
+                      <SvgSelector
+                        id="delete-basket"
+                        classNames="admin-delete-pr-btn"
+                        onClick={() => setDeleteModalActive(true)}
+                      />
+                    </div>
+                    <Modal active={editModalActive} setActive={setEditModalActive}>
+                      <AddEditModalContent
+                        variant="editContent"
+                        product={currentProduct}
+                        setModalActive={setEditModalActive}
+                      />
+                    </Modal>
+                    <Modal active={deleteModalActive} setActive={setDeleteModalActive}>
+                      <DeleteModalContent productId={productId} setModalActive={setDeleteModalActive} />
+                    </Modal>
+                  </>
                 ) : null
             }
           </div>
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import Modal from '../components/Modal/Modal';
+import AddEditModalContent from '../components/ModalContent/AddEditModalContent/AddEditModalContent';
 import ProductList from '../components/ProductList/ProductList';
 import SvgSelector from '../components/SvgSelector';
 import { Roles } from '../utils/consts';
@@ -9,23 +11,32 @@ function Store() {
   const products = useSelector((state: any) => state.products.products);
   const userRole = useSelector((state: any) => state.user.role);
 
+  const [modalActive, setModalActive] = useState(false);
+
   return (
-    <div className="storePage">
+    <div className="store-page">
       <div className="container">
-        <div className="bigTitle">Уютный магазин книжных мелочей</div>
+        <div className="big-title">Уютный магазин книжных мелочей</div>
         {
           products.length
             ? <ProductList products={products} />
-            : <div className="emptyMessage">Продуктов нет</div>
+            : <div className="empty-message">Продуктов нет</div>
         }
         {
           userRole === Roles.ADMIN
           && (
-            <div className="storeAddBtn">
-              <SvgSelector id="plus-add-icon" />
-            </div>)
+            <>
+              <div className="store-add-btn">
+                <SvgSelector id="plus-add-icon" onClick={() => setModalActive(true)} />
+              </div>
+              <Modal active={modalActive} setActive={setModalActive}>
+                <AddEditModalContent variant="addContent" setModalActive={setModalActive} />
+              </Modal>
+            </>
+          )
         }
       </div>
+
     </div>
   );
 }
